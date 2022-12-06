900a9e4e61b0c1ead56a55fb5362c0be874e5779
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-06 13:29:25 +0330

    Home | views.py | create HomeView

--- shop_django/M/home/views.py
+++ shop_django/M/home/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/home.html')
+
+
