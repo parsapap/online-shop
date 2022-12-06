f3486f1193aedc104c561a548990a57bb3efc4fa
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-07 02:04:11 +0330

    Home | views.py | Create StoreView

--- shop_django/M/home/views.py
+++ shop_django/M/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-
+from .models import Product
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'home/home.html')
+
+
+class StoreView(View):
+    def get(self, request):
+        products = Product.objects.filter(available=True)
+        return render(request, 'home/index.html', {'products': products})
