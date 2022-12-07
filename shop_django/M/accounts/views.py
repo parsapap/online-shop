211d798610eaa1ce37cf023808ca1f006fee837e
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-07 19:09:35 +0330

    Accounts | views.py | Create class about UserLoginView

--- shop_django/M/accounts/views.py
+++ shop_django/M/accounts/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm, VerifyCodeForm
+from .forms import UserRegistrationForm, VerifyCodeForm, UserLoginForm
 from django.views import View
 import random
 from utils import send_otp_code
@@ -56,3 +57,24 @@ class UserRegisterVerifyCodeView(View):
                 messages.error(request, 'this code is wrong', 'danger')
                 return redirect('accounts:verify_code')
         return redirect('home:home')
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, phone_number=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'info')
+                return redirect('home:home')
+            messages.error(request, 'phone or password is wrong', 'warning')
+        return render(request, self.template_name, {'form': form})
