7f85b68aa951cc7ec25eb846b50961c465ad4d53
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-07 18:51:49 +0330

    M | settings.py | change debug and allowed host

--- shop_django/M/M/settings.py
+++ shop_django/M/M/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$x3^f@t5n8l$w)pf)!aj&%d%=k#p!8$ud+rwq#0m$ef+sce!w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
