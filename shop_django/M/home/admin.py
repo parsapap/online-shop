f5e40bcd5512bcaac139f98c734fce0aa8611c10
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-09 20:29:31 +0330

    Home | admin.py | Create ProductAdmin and CategoryAdmin class

--- shop_django/M/home/admin.py
+++ shop_django/M/home/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Comment
+from core.admin import BaseAdmin
 
-admin.site.register(Category)
-admin.site.register(Product)
 
+@admin.register(Product)
+class ProductAdmin(BaseAdmin):
+    list_display = ['name', 'slug', 'category', 'price', 'available', 'created', 'updated', 'is_active', 'is_deleted']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Comment)
+class CommentAdmin(BaseAdmin):
+    list_display = ['user', 'product', 'body', 'created', 'updated', 'is_active', 'is_deleted']
