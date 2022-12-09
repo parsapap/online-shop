24d92c80c15b02c16a453eaa50837b9c87183080
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-09 16:28:21 +0330

    Home | models.py | Create Comment class

--- shop_django/M/home/models.py
+++ shop_django/M/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import User
 
 
 class Category(models.Model):
@@ -34,3 +35,15 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('home:product_detail', args=[self.slug])
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='pcomments')
+    reply = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
