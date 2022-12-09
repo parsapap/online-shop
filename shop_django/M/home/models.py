e9271409b8e562af22b870d6a3a6b55694d4ce4a
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-09 20:30:40 +0330

    Home | models.py | Change Category models

--- shop_django/M/home/models.py
+++ shop_django/M/home/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.urls import reverse
 from accounts.models import User
+from core.models import BaseModel
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='scategory', null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -16,7 +19,9 @@ class Category(models.Model):
         return self.name
 
 
-class Product(models.Model):
+
+
+class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
@@ -24,8 +29,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('name',)
@@ -37,13 +40,12 @@ class Product(models.Model):
         return reverse('home:product_detail', args=[self.slug])
 
 
-class Comment(models.Model):
+class Comment(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='pcomments')
     reply = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
     is_reply = models.BooleanField(default=False)
     body = models.TextField(max_length=400)
-    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user} - {self.body[:30]}'
