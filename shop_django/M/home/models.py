3b7636385bc956df8dc9ee15cad08b9ed037fdf5
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-14 15:50:10 +0330

    Home | models.py | Create get_absolut_url for Category models

--- shop_django/M/home/models.py
+++ shop_django/M/home/models.py
@@ -18,6 +18,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('home:category_filter', args=[self.slug])
+
 
 
 
