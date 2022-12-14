8a86fb25ab38da6307afa93d024740ff12d88f74
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-14 15:54:22 +0330

    Home | views.py | Create ProductDetailView

--- shop_django/M/home/views.py
+++ shop_django/M/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from .models import Product
+from .models import Product, Category
+
 
 class HomeView(View):
     def get(self, request):
@@ -8,6 +9,16 @@ class HomeView(View):
 
 
 class StoreView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/index.html', {'products': products})
+        categories = Category.objects.all()
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'home/index.html', {'products': products, 'categories': categories})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/product_detail.html', {'product': product})
