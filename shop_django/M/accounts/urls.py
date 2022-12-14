3e085921dc0d4c5cdf52c063d640dc4eac14f743
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-14 15:40:47 +0330

    Accounts | urls.py | create pathes about forget password

--- shop_django/M/accounts/urls.py
+++ shop_django/M/accounts/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
+    path('verify/', views.UserRegisterVerifyCodeView.as_view(), name='verify_code'),
+    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('confirm/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
