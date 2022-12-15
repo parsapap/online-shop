d93cf0bc83a17cb7e238c3d7a54a6a93f99b4e69
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-15 16:28:04 +0330

    Accounts | admin.py | Change permissions of fields and edit filter_horizontal

--- shop_django/M/accounts/admin.py
+++ shop_django/M/accounts/admin.py
@@ -19,7 +19,8 @@ class UserAdmin(BaseUserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'phone_number', 'full_name', 'password')}),
-        ('Permissions', {'fields': ('is_active', 'is_admin', 'last_login')}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
     )
 
     add_fieldsets = (
@@ -28,7 +29,7 @@ class UserAdmin(BaseUserAdmin):
     #
     search_fields = ('email', 'full_name')
     ordering = ('full_name',)
-    filter_horizontal = ()
+    filter_horizontal = ('groups', 'user_permissions')
 
 
 admin.site.unregister(Group)
