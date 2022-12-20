0eeca06b140b626f18328324629ab77cd5de0b66
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-20 03:32:10 +0330

    Home | admin.py | Edit productadmin

--- shop_django/M/home/admin.py
+++ shop_django/M/home/admin.py
@@ -5,11 +5,12 @@ from core.admin import BaseAdmin
 
 @admin.register(Product)
 class ProductAdmin(BaseAdmin):
-    list_display = ['name', 'slug', 'category', 'price', 'available', 'created', 'updated', 'is_active', 'is_deleted']
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'is_active', 'is_deleted']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ['name', 'slug']
+    raw_id_fields = ('category',)
 
 
 @admin.register(Category)
