8aeae24ff5adb8c38e6ce72c40638bb22c0a5681
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-23 13:53:55 +0330

    Orders | cart.py | Change cart.py and Add methods

--- shop_django/M/orders/cart.py
+++ shop_django/M/orders/cart.py
@@ -11,4 +11,23 @@ class Cart:
             cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
 
+        for item in cart.values():
+            item['total_price'] = int(float(item['price'])) * item['quantity']
+            yield item
+
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
