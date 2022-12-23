4c3bf55131de70ab4d4b64d681d61edfdfb22b05
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-23 13:44:44 +0330

    Orders | views.py | Create CardView

--- shop_django/M/orders/views.py
+++ shop_django/M/orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from .cart import Cart
+from home.models import Product
+from .forms import CardAddForm
 
-# Create your views here.
+
+class CartView(View):
+    def get(self, request):
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart': cart})
+
+
+
