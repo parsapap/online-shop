badccc35033418f1305846b26d83cb9075d321ce
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-12-23 13:31:34 +0330

    Home | views.py | Add CardAddForm to view

--- shop_django/M/home/views.py
+++ shop_django/M/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Product, Category
+from orders.forms import CardAddForm
 
 
 class HomeView(View):
@@ -11,7 +12,7 @@ class HomeView(View):
 class StoreView(View):
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        categories = Category.objects.all()
+        categories = Category.objects.filter(is_sub=False)
         if category_slug:
             category = get_object_or_404(Category, slug=category_slug)
             products = products.filter(category=category)
@@ -21,4 +22,5 @@ class StoreView(View):
 class ProductDetailView(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
-        return render(request, 'home/product_detail.html', {'product': product})
+        form = CardAddForm()
+        return render(request, 'home/product_detail.html', {'product': product, 'form': form})
