1fb8f831433ca8040d81d177b68f559683f70f9b
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-03 15:47:45 +0330

    Orders | models.py | Make get_total_price and get_cost for Order and OrderItem models

--- shop_django/M/orders/models.py
+++ shop_django/M/orders/models.py
@@ -18,6 +18,9 @@ class Order(BaseModel):
     def __str__(self):
         return f'{self.user} - {str(self.id)}'
 
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all())
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
@@ -27,6 +30,9 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
 
 
 class Coupon(models.Model):
