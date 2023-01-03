2fb2679bd581697ffe72d58f4a17a557d3828f3b
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-03 13:07:29 +0330

    API | views.py | Make ProfileApiView

--- shop_django/M/api/views.py
+++ shop_django/M/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.models import User
+from .serializers import ProfileSerializer
+from rest_framework import status
 
-# Create your views here.
+
+# account app api views
+class ProfileApiView(APIView):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        ser_data = ProfileSerializer(instance=user)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+
+class ProfileEditApiView(APIView):
+    pass
