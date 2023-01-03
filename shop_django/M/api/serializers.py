95b69b4b33d6da50ab99a8eb1e57522bec8ed6a4
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-03 15:55:52 +0330

    API | serializers.py | Make ProductListSerializer and CategoryListSerializer

--- shop_django/M/api/serializers.py
+++ shop_django/M/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from accounts.models import User
+from home.models import Product, Category
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -11,3 +12,21 @@ class ProfileSerializer(serializers.ModelSerializer):
 class ProfileEditSerializer(serializers.ModelSerializer):
     model = User
     fields = ['full_name', 'email', 'phone_number', 'address']
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'image', 'category', 'description', 'available']
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug', 'is_sub', 'sub_category']
+
+
+class ProductDetailSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'description', 'image']
