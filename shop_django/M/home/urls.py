128387704c50047d495e16896084b0053189ac27
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-03 13:09:31 +0330

    Home | urls.py | Edit path

--- shop_django/M/home/urls.py
+++ shop_django/M/home/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('store/', views.StoreView.as_view(), name='store'),
     path('category/<slug:category_slug>/', views.StoreView.as_view(), name='category_filter'),
-    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 
 ]
