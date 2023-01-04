e812a2ec913dcaeff72a4b4d61054f557fc66836
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-05 02:23:50 +0330

    Order | views.py | Create OrderDetailView

--- shop_django/M/orders/views.py
+++ shop_django/M/orders/views.py
@@ -1,9 +1,15 @@
+import json
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from home.models import Product
 from .forms import CardAddForm
 from .models import Order, OrderItem, Coupon
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+import requests
+import json
 
 
 class CartView(View):
@@ -30,11 +36,20 @@ class CartRemoveView(View):
         return redirect('orders:cart')
 
 
-class OrderCreateView(View):
+class OrderCreateView(LoginRequiredMixin, View):
     def get(self, request):
         cart = Cart(request)
         order = Order.objects.create(user=request.user)
         for item in cart:
-            OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+            OrderItem.objects.create(order=order, product=item['product'], price=float(item['price']),
                                      quantity=item['quantity'])
-            return redirect('orders:order_detail', order.id)
+        cart.clear()
+        return redirect('orders:order_detail', order.id)
+
+
+class OrderDetailView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        return render(request, 'orders/order.html', {'order': order})
+
+
