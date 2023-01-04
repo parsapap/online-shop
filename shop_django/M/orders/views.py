820d9b5407a589de38ceab45ef477938fcf28088
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-05 02:24:26 +0330

    Order | views.py | Create OrderPayView for zarinpal

--- shop_django/M/orders/views.py
+++ shop_django/M/orders/views.py
@@ -53,3 +53,39 @@ class OrderDetailView(LoginRequiredMixin, View):
         return render(request, 'orders/order.html', {'order': order})
 
 
+MERCHANT = '41b3a45225a8c34f34186d8484ba6e87413d'
+ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
+amount = 11000  # Rial / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = 'email@example.com'  # Optional
+mobile = '09123456789'  # Optional
+# Important: need to edit for realy server.
+CallbackURL = 'http://127.0.0.1:8000/orders/verify/'
+
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order.id,
+        }
+        req_data = {
+            "merchant_id": MERCHANT,
+            "amount": 100000,
+            "callback_url": CallbackURL,
+            "description": description,
+            "metadata": {"mobile": request.user.phone_number, "email": request.user.email}
+        }
+        req_header = {"accept": "application/json",
+                      "content-type": "application/json'"}
+        req = requests.post(url=ZP_API_REQUEST, data=json.dumps(
+            req_data), headers=req_header)
+        authority = req.json()['data']['authority']
+        if len(req.json()['errors']) == 0:
+            return redirect(ZP_API_STARTPAY.format(authority=authority))
+        else:
+            e_code = req.json()['errors']['code']
+            e_message = req.json()['errors']['message']
+            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
