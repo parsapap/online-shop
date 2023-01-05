5c62fb0f54129b07d3a42d2e92cab9c06106d0ae
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-05 16:12:54 +0330

    Order | forms.py | Create CouponApplyForm

--- shop_django/M/orders/forms.py
+++ shop_django/M/orders/forms.py
@@ -5,3 +5,12 @@ class CardAddForm(forms.Form):
     quantity = forms.IntegerField(label='', min_value=1, max_value=9,
                                   widget=forms.NumberInput(
                                       attrs={'class': 'form-control w-25', 'placeholder': 'Quantity'}))
+
+
+class CouponApplyForm(forms.Form):
+    code = forms.CharField(label='Code', widget=forms.TextInput(attrs={'class': 'mb-0 text-primary pe-1'}))
+
+
+
+
+
