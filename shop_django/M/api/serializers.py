a2393e2bc7a93afd1582699b2c77ea9e23af9887
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2023-01-08 19:12:54 +0330

    API | serializers.py | Create serializers fpr OrderItem and Order

--- shop_django/M/api/serializers.py
+++ shop_django/M/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from accounts.models import User
 from home.models import Product, Category, Comment
+from orders.models import Order, OrderItem
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -48,3 +49,21 @@ class CommentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['user', 'product', 'reply', 'is_reply', 'body']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = ('order_items', 'user', 'paid', 'discount')
+
+    def get_order_items(self, obj):
+        order_items = obj.items.all()
+        return OrderItemSerializer(order_items, many=True).data
